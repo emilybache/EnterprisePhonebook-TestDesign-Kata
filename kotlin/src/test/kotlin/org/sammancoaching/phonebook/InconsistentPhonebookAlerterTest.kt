b74c5d033daa02f48ba2211262206ab03a03d639
emily <emily@bacheconsulting.com>
2025-10-19 14:10:30 +0200

    reset to starting position

--- kotlin/src/test/kotlin/org/sammancoaching/phonebook/InconsistentPhonebookAlerterTest.kt
+++ kotlin/src/test/kotlin/org/sammancoaching/phonebook/InconsistentPhonebookAlerterTest.kt
@@ -7,20 +7,5 @@ import kotlin.test.assertEquals
 
 @ExtendWith(HttpServerLifecycle::class)
 class InconsistentPhonebookAlerterTest {
-    @Test
-    fun triggerAlertOnPotentialClash() {
-        val server = HttpServerLifecycle.httpServer!!
-        val alerter = InconsistentPhonebookAlerter(server.url)
 
-        val event = BadPhonebookEntryEvent(
-            2,
-            "Bob",
-            "1234",
-            Pair("Ted", "123")
-        )
-        alerter.sendAlert(event)
-
-        assertEquals("""{"size":"2","new_entry":"Bob cannot be added with number 1234","clash":"Ted with number 123"}""",
-            server.getHandler().getLatestRequestBody())
-    }
 }
--- kotlin/src/test/kotlin/org/sammancoaching/phonebook/SecurityClearanceAuthorizerTest.kt
+++ kotlin/src/test/kotlin/org/sammancoaching/phonebook/SecurityClearanceAuthorizerTest.kt
@@ -8,24 +8,5 @@ import kotlin.test.assertEquals
 
 @ExtendWith(HttpServerLifecycle::class)
 class SecurityClearanceAuthorizerTest {
-    @Test
-    fun whenRemoteServiceReturns200_isAuthorized() {
-        val server = HttpServerLifecycle.httpServer!!
-        val authorizer = SecurityClearanceAuthorizer(server.url)
 
-        val isAuthorized = authorizer.isAuthorized
-
-        assertEquals(true, isAuthorized)
-    }
-
-    @Test
-    fun whenRemoteServiceReturns404_isNotAuthorized() {
-        val server = HttpServerLifecycle.httpServer!!
-        server.getHandler().setResponse(404, "Not found")
-        val authorizer = SecurityClearanceAuthorizer(server.url)
-
-        val isAuthorized = authorizer.isAuthorized
-
-        assertEquals(false, isAuthorized)
-    }
 }
