d0842ab732b1c0898acec5d084026321a13abfb4
emily <emily@bacheconsulting.com>
2025-10-27 09:07:51 +0100

    reset to starting position java

--- java/src/test/java/org/sammancoaching/InconsistentPhonebookAlerterTest.java
+++ java/src/test/java/org/sammancoaching/InconsistentPhonebookAlerterTest.java
@@ -13,29 +13,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(HttpServerLifecycle.class)
 public class InconsistentPhonebookAlerterTest {
 
-    private ObjectMapper mapper = new ObjectMapper();
-
-    @Test
-    void testEventToJsonData() {
-        BadPhonebookEntryEvent event = new BadPhonebookEntryEvent(10, "Ted", "1234", Map.entry("Bob", "1234"));
-        AlertData data = event.eventToAlert();
-        
-        assertEquals("10", data.size());
-        assertEquals("Ted cannot be added with number 1234", data.newEntry());
-        assertEquals("Bob with number 1234", data.clash());
-    }
-
-    @Test
-    void testGenerateEvent() throws Exception {
-        LightweightHttpServer httpServer = HttpServerLifecycle.getHttpServer();
-        InconsistentPhonebookAlerter alerter = new InconsistentPhonebookAlerter(httpServer.getUrl());
-        BadPhonebookEntryEvent event = new BadPhonebookEntryEvent(10, "Ted", "1234", Map.entry("Bob", "1234"));
-
-        alerter.sendAlert(event);
-
-        assertEquals("PUT", httpServer.getHandler().getLatestRequestType());
-        String receivedDataAsString = httpServer.getHandler().getLatestRequestBody();
-        AlertData receivedData = mapper.readValue(receivedDataAsString, AlertData.class);
-        assertEquals(event.eventToAlert(), receivedData);
-    }
 }
--- java/src/test/java/org/sammancoaching/SecurityClearanceAuthorizerTest.java
+++ java/src/test/java/org/sammancoaching/SecurityClearanceAuthorizerTest.java
@@ -10,27 +10,4 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(HttpServerLifecycle.class)
 public class SecurityClearanceAuthorizerTest {
 
-    @Test
-    void testAuthorize() {
-        LightweightHttpServer webserver = HttpServerLifecycle.getHttpServer();
-        SecurityClearanceAuthorizer authorizer = new SecurityClearanceAuthorizer(webserver.getUrl());
-        webserver.getHandler().setResponse(200, "OK");
-
-        boolean result = authorizer.isAuthorized();
-
-        assertTrue(result);
-        assertEquals("GET", webserver.getHandler().getLatestRequestType());
-    }
-
-    @Test
-    void testNotAuthorize() {
-        LightweightHttpServer webserver = HttpServerLifecycle.getHttpServer();
-        SecurityClearanceAuthorizer authorizer = new SecurityClearanceAuthorizer(webserver.getUrl());
-        webserver.getHandler().setResponse(404, "Not Found");
-
-        boolean result = authorizer.isAuthorized();
-
-        assertFalse(result);
-        assertEquals("GET", webserver.getHandler().getLatestRequestType());
-    }
 }
